65d27ddf30c772690f9bb5c359882a172547511c
eshom <48328872+eshom@users.noreply.github.com>
2024-05-17 12:12:44 +0300

    format day 1 code

--- cmd/day1/day1.go
+++ cmd/day1/day1.go
@@ -10,49 +10,49 @@ import (
 )
 
 func main() {
-    // part 1
-    var input string = readInput("cmd/day1/data/input.txt")
-    var calories []string = strings.Split(input, "\n")
-    var sums []int = sumCals(calories)
-    var maxCal int = maxSum(sums)
-    log.Println(maxCal)
+	// part 1
+	var input string = readInput("cmd/day1/data/input.txt")
+	var calories []string = strings.Split(input, "\n")
+	var sums []int = sumCals(calories)
+	var maxCal int = maxSum(sums)
+	log.Println(maxCal)
 
-    // part 2
-    var top3 []int = sums
-    slices.Sort(top3)
-    slices.Reverse(top3)
-    assert.Assert(len(top3) >= 3, "must have at least 3 sums")
-    top3 = top3[0:3]
-    log.Println(top3[0] + top3[1] + top3[2])
+	// part 2
+	var top3 []int = sums
+	slices.Sort(top3)
+	slices.Reverse(top3)
+	assert.Assert(len(top3) >= 3, "must have at least 3 sums")
+	top3 = top3[0:3]
+	log.Println(top3[0] + top3[1] + top3[2])
 }
 
 func readInput(filename string) string {
-    contents, err := os.ReadFile(filename)
-    assert.NoError(err)
-    return string(contents)
+	contents, err := os.ReadFile(filename)
+	assert.NoError(err)
+	return string(contents)
 }
 
 func sumCals(cals []string) []int {
-    out := make([]int, 0, 1000)
-    var sum int = 0
-    for _, str := range cals {
-        if str != "\n" && str != "" {
-            num, err := strconv.Atoi(str)
-            assert.NoError(err)
-            sum += num
-        } else {
-            out = append(out, sum)
-            sum = 0
-        }
-    }
+	out := make([]int, 0, 1000)
+	var sum int = 0
+	for _, str := range cals {
+		if str != "\n" && str != "" {
+			num, err := strconv.Atoi(str)
+			assert.NoError(err)
+			sum += num
+		} else {
+			out = append(out, sum)
+			sum = 0
+		}
+	}
 
-    return out
+	return out
 }
 
 func maxSum(sums []int) int {
-    var out int = 0
-    for _, s := range sums {
-        out = max(out, s)
-    }
-    return out
+	var out int = 0
+	for _, s := range sums {
+		out = max(out, s)
+	}
+	return out
 }
